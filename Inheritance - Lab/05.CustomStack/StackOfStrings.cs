f385b8d660e876469c8b435d64da3a9ef1b77190
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-14 18:18:21 +0300

    04.CustomRandomList

---
+++ Inheritance - Lab/05.CustomStack/StackOfStrings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (Count <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            Stack<string> result = new();
+            foreach (var item in range)
+            {
+                result.Push(item);
+            }
+            return result;
+        }
+    }
+}
