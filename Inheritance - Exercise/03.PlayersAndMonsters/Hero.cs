53c5f1e1151824ef554462f8a555447d848ee0b7
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-15 14:16:32 +0300

    03.PlayersAndMonsters

---
+++ Inheritance - Exercise/03.PlayersAndMonsters/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        public Hero(string name, int level)
+        {
+            Username = name;
+            Level = level;
+        }
+
+        public string Username { get; set; }
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return $"Type: {GetType().Name} Username: {Username} Level: {Level}";
+        }
+    }
+}
