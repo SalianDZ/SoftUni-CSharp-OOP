39317167d2420095420753ab5ffb925ea4be1346
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-15 20:43:15 +0300

    04.NeedForSpeed

---
+++ Inheritance - Exercise/04.NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double defaultFuelConsumption = 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption => defaultFuelConsumption;
+    }
+}
---
+++ Inheritance - Exercise/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double defaultFuelConsupmtion = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+        public virtual double FuelConsumption => defaultFuelConsupmtion;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            double consumedFuel = kilometers * FuelConsumption;
+            Fuel -= consumedFuel;
+        }
+    }
+}
