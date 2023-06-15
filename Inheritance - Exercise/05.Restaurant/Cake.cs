c2770a738ac1029905a15fe625c23776ccc77cc5
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-15 21:16:53 +0300

    05.Restaurant

---
+++ Inheritance - Exercise/05.Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal cakePrice = 5m;
+        private const double cakeGrams = 250;
+        private const double cakeCalories = 1000;
+        public Cake(string name) : base(name, cakePrice, cakeGrams, cakeCalories)
+        {
+        }
+    }
+}
---
+++ Inheritance - Exercise/05.Restaurant/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal coffeePrice = 3.50m;
+        private const double coffeeMilliliters = 50;
+        public Coffee(string name, double caffeine) : base(name, coffeePrice, coffeeMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+        public double Caffeine { get; set; }
+    }
+}
