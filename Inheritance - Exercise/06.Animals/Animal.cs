9519c5329aa0f3b153d7926894a7758d9d5be44c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-16 18:04:24 +0300

    06.Animals

---
+++ Inheritance - Exercise/06.Animals/Animal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        protected string name;
+        protected int age;
+        protected string gender;
+        
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (value == "" || value == " ")
+                {
+                    name = "Invalid input!";
+                    return;
+                }
+
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+            }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == "" || value == " ")
+                {
+                    gender = "Invalid input!";
+                    return;
+                }
+
+                gender = value;
+            }
+        }
+
+        public virtual string ProduceSound()
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new();
+            string sound = ProduceSound();
+            if (sound == "Woof!")
+            {
+                result.AppendLine($"Dog");
+            }
+            else if (sound == "Meow meow")
+            {
+                result.AppendLine($"Cat");
+            }
+            else if (sound == "Ribbit")
+            {
+                result.AppendLine($"Frog");
+            }
+            else if(sound == "Meow")
+            {
+                result.AppendLine($"Kitten");
+            }
+            else if (sound == "MEOW")
+            {
+                result.AppendLine($"Tomcat");
+            }
+            result.AppendLine($"{Name} {Age} {Gender}");
+            result.AppendLine($"{ProduceSound()}");
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Inheritance - Exercise/06.Animals/StartUp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            bool wrongData = false;
+            List<Animal> animals = new List<Animal>();
+            while (true)
+            {
+                string animal = Console.ReadLine();
+                if (animal == "Beast!")
+                {
+                    break;
+                }
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (animal == "Cat")
+                {
+                    Cat cat = new(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                    if (cat.Name == "" || cat.Name == " " || cat.Gender == "" ||cat.Gender == " " || cat.Age < 0)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    animals.Add(cat);
+                }
+                else if (animal == "Dog")
+                {
+                    Dog dog = new(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                    if (dog.Name == "" || dog.Name == " " || dog.Gender == "" || dog.Gender == " " || dog.Age < 0)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    animals.Add(dog);
+                }
+                else if (animal == "Frog")
+                {
+                    Frog frog = new(tokens[0], int.Parse(tokens[1]), tokens[2]);
+                    if (frog.Name == "" || frog.Name == " " || frog.Gender == "" || frog.Gender == " " || frog.Age < 0)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    animals.Add(frog);
+                }
+                else if (animal == "Tomcat")
+                {
+                    Tomcat cat = new(tokens[0], int.Parse(tokens[1]));
+                    if (cat.Name == "" || cat.Name == " " || cat.Gender == "" || cat.Gender == " " || cat.Age < 0)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    animals.Add(cat);
+                }
+                else if (animal == "Kitten" || animal == "Kittens")
+                {
+                    Tomcat cat = new(tokens[0], int.Parse(tokens[1]));
+                    if (cat.Name == "" || cat.Name == " " || cat.Gender == "" || cat.Gender == " " || cat.Age < 0)
+                    {
+                        Console.WriteLine("Invalid input!");
+                        continue;
+                    }
+                    animals.Add(cat);
+                }
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
