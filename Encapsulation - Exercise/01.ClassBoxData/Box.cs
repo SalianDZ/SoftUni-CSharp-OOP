28ac9ebfdc12fe4670cb55e3146f15878e16e0a5
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-19 18:38:17 +0300

    Encapsulation - Exercise

---
+++ Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+		private double length;
+		private double width;
+		private double height;
+
+		public Box(double length, double width, double height)
+		{
+			Height = height;
+			Width = width;
+			Length = length;
+		}
+
+		public double Height
+        {
+			get { return height; }
+			set 
+			{
+				ValidateNumber(value, "Height");
+				height = value;
+			}
+		}
+
+
+		public double Width
+        {
+			get { return width; }
+			set
+			{
+				ValidateNumber(value, "Width");
+				width = value;
+			}
+		}
+
+
+		public double Length
+        {
+			get { return length; }
+			set
+			{
+				ValidateNumber(value, "Length");
+				length = value;
+			}
+		}
+
+		public double SurfaceArea()
+		{
+			double result = (2 * Length * Width) + (2 * Length * Height) + (2 * Width * Height);
+			return result;
+		}
+
+		public double Volume()
+		{
+			double result = Length * Width * Height;
+			return result;
+		}
+
+		public double LateralSurfaceArea()
+		{
+			double result = (2 * Length * Height) + (2 * Width * Height);
+			return result;
+		}
+
+		private void ValidateNumber(double value, string propertyName)
+		{
+			if (value <= 0)
+			{
+				 ArgumentException ex = new ArgumentException($"{propertyName} cannot be zero or negative.");
+				throw ex;
+			}
+		}
+
+	}
+}
---
+++ Encapsulation - Exercise/01.ClassBoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassBoxData 
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new(length, width, height);
+                double surfaceArea = box.SurfaceArea();
+                double volume = box.Volume();
+                double lateralSurfaceArea = box.LateralSurfaceArea();
+                Console.WriteLine($"Surface Area - {surfaceArea:F2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {volume:f2}");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
