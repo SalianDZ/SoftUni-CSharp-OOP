7acd9dc4203521ec1f898b00d7a35b2b331663af
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-06-30 18:49:32 +0300

    Exceptions and Error Handling - Lab

---
+++ Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+	int num = int.Parse(Console.ReadLine());
+	CheckNumber(num);
+
+	Console.WriteLine(Math.Sqrt(num));
+}
+catch (ArgumentException ex)
+{
+	Console.WriteLine(ex.Message);
+}
+finally
+{
+	Console.WriteLine("Goodbye.");
+}
+
+
+
+void CheckNumber(int num)
+{
+	if (num < 0)
+	{
+		throw new ArgumentException("Invalid number.");
+	}
+}
