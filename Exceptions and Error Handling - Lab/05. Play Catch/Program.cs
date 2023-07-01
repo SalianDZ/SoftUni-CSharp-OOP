577c6bceb642cf0d8c7c32cfed7612d8415014f7
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-07-01 11:33:04 +0300

    05. Play Catch

---
+++ Exceptions and Error Handling - Lab/05. Play Catch/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int exceptionCounter = 0;
+
+while (exceptionCounter < 3)
+{
+    string[] command = Console.ReadLine().Split();
+
+    try
+    {
+        if (command[0] == "Replace")
+        {
+            int index = int.Parse(command[1]);
+            int element = int.Parse(command[2]);
+
+            nums[index] = element;
+        }
+        else if (command[0] == "Print")
+        {
+            int firstIndex = int.Parse(command[1]);
+            int secondIndex = int.Parse(command[2]) + 1;
+
+            Console.WriteLine(String.Join(", ", nums[firstIndex..secondIndex]));
+        }
+        else if (command[0] == "Show")
+        {
+            int index = int.Parse(command[1]);
+            Console.WriteLine(nums[index]);
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("The variable is not in the correct format!");
+        exceptionCounter++;
+    }
+    catch (ArgumentOutOfRangeException)
+    {
+        Console.WriteLine("The index does not exist!");
+        exceptionCounter++;
+    }
+    catch (IndexOutOfRangeException)
+    {
+        Console.WriteLine("The index does not exist!");
+        exceptionCounter++;
+
+}
+Console.WriteLine(String.Join(", ", nums));
