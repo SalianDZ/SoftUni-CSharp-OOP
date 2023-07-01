a0ca318c19e56b21cdce81543c283c12c320ca94
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-07-01 11:04:44 +0300

    04. Sum of Integers

---
+++ Exceptions and Error Handling - Lab/04. Sum of Integers/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] nums = Console.ReadLine().Split();
+int sum = 0;
+int currentNumber = 0;
+string currentNumberAsString = string.Empty;
+for (int i = 0; i < nums.Length; i++)
+{
+    try
+    {
+        currentNumberAsString = nums[i];
+        currentNumber = int.Parse(nums[i]);
+
+        sum += currentNumber;
+        Console.WriteLine($"Element '{currentNumber}' processed - current sum: {sum}");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{currentNumberAsString}' is in wrong format!");
+        Console.WriteLine($"Element '{currentNumberAsString}' processed - current sum: {sum}");
+        continue;
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{currentNumberAsString}' is out of range!");
+        Console.WriteLine($"Element '{currentNumberAsString}' processed - current sum: {sum}");
+        continue;
+    }
+}
+
+
+
+Console.WriteLine($"The total sum of all integers is: {sum}");
