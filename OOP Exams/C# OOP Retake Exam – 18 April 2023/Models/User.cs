56561d6efb5574a49658535c8ce36879105df7a6
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-07-04 19:08:05 +0300

    C# OOP Retake Exam – 18 April 2023

---
+++ OOP Exams/C# OOP Retake Exam – 18 April 2023/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using EDriveRent.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDriveRent.Utilities.Messages;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.FirstNameNull);
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName => throw new NotImplementedException();
+
+        public double Rating => throw new NotImplementedException();
+
+        public string DrivingLicenseNumber => throw new NotImplementedException();
+
+        public bool IsBlocked => throw new NotImplementedException();
+
+        public void DecreaseRating()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncreaseRating()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
