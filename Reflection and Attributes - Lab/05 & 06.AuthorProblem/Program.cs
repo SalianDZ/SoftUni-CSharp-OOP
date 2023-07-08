4d3af72c8f2c0570ef8b8276fc50121120ca6436
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-07-09 01:05:24 +0300

    05 & 06.AuthorProblem

---
+++ Reflection and Attributes - Lab/05 & 06.AuthorProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthorProblem
+{
+    [Author("Viktor")]
+    public class StartUp
+    {
+        [Author("Geroge")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/05 & 06.AuthorProblem/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            foreach (var method in type.GetMethods((BindingFlags) 60))
+            {
+                AuthorAttribute[] attributes = method.GetCustomAttributes<AuthorAttribute>().ToArray();
+
+                foreach (var attribute in attributes)
+                {
+                    Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                }
+            }
+        }
+    }
+}
