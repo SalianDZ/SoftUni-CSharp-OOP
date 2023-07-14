bda820b3e6c490806b62f07db46dd2db6a05e064
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-07-15 01:58:40 +0300

    Unit Testing - Exercises

--- Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
+++ Unit Testing - Exercises/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,77 @@
 ﻿namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class ArenaTests
     {
+        Arena arena;
+        [SetUp]
+        public void SetUp()
+        {
+            arena = new Arena();
+        }
 
+        [TearDown]
+        public void TearDown()
+        {
+            arena = null;
+
+        }
+        [Test]
+        public void ArenaConstructorTest()
+        {
+            Assert.AreEqual(0, arena.Count);
+        }
+
+        [Test]
+        public void TestIfWarriorGetterWorks()
+        {
+            Warrior warrior = new("Gosho", 50, 150);
+            arena.Enroll(warrior);
+            List<Warrior> warriors = new List<Warrior>();
+            warriors.Add(warrior);
+            Assert.That(warriors, Is.EquivalentTo(arena.Warriors));
+        }
+
+        [Test]
+        public void TestIfEnrollMethodWorksProperly()
+        {
+            Warrior warrior = new("Gosho", 50, 150);
+            arena.Enroll(warrior);
+
+            Assert.AreEqual(arena.Count, 1);
+        }
+
+        [Test]
+        public void TestIfEnrollMethodThrowsAnExceptionIfThereIsAlreadyAWarriorWithThisName()
+        {
+            Warrior warrior = new("Gosho", 50, 150);
+            arena.Enroll(warrior);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(new Warrior("Gosho", 40, 120)));
+        }
+
+        [Test]
+        public void TestIfFightMethodThrowsAnExceptionIfAttackerOrDefenderNameIsNull()
+        {
+            Warrior attacker = new("Pesho", 30, 100);
+            arena.Enroll(attacker);
+            Assert.Throws<InvalidOperationException>(() => arena.Fight(attacker.Name, "Gosho"));
+        }
+
+        [Test]
+        public void TestIfFightMethodWorksProperly ()
+        {
+            Warrior attacker = new("Pesho", 30, 100);
+            Warrior deffener = new("Gosho", 20, 150);
+            arena.Enroll(attacker);
+            arena.Enroll(deffener);
+            arena.Fight(attacker.Name, deffener.Name);
+            Assert.AreEqual(attacker.HP, 80);
+            Assert.AreEqual(deffener.HP, 120);
+        }
     }
 }
--- Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
+++ Unit Testing - Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,103 @@
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class WarriorTests
     {
+        private Warrior warrior;
+        [SetUp]
+        public void SetUp()
+        {
+            warrior = new("Pesho", 50, 100);
+        }
 
+        [TearDown]
+        public void TearDown()
+        {
+            warrior = null;
+        }
+
+        [Test]
+        public void WarriorConstructorTest()
+        {
+            Assert.AreEqual("Pesho", warrior.Name);
+            Assert.AreEqual(50, warrior.Damage);
+            Assert.AreEqual(100, warrior.HP);
+        }
+
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void TestIfNameThrowsAnExceptionWhenNameIsNullOrEmptyOrWhitespace(string name)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, 50, 100));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-6)]
+        public void TestIfDamageThrowsAnExceptionWhenInputIsZeroOrNegative(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Pesho", damage, 100));
+        }
+
+        [Test]
+        public void TestIfHPThrowsAnExceptionWhenInputIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Pesho", 50, -10));
+        }
+
+        [Test]
+        public void TestIfAttackMethodActuallyAttacksOtherWarriorWhenWeDontKillEnemy()
+        {
+            Warrior enemy = new("Gosho", 20, 200);
+            warrior.Attack(enemy);
+
+            Assert.AreEqual(80, warrior.HP);
+            Assert.AreEqual(150, enemy.HP);
+        }
+
+        [Test]
+        public void TestIfAttackMethodActuallyAttacksOtherWarriorWhenWeKillEnemy()
+        {
+            Warrior enemy = new("Gosho", 20, 40);
+            warrior.Attack(enemy);
+
+            Assert.AreEqual(80, warrior.HP);
+            Assert.AreEqual(0, enemy.HP);
+        }
+
+
+        [Test]
+        [TestCase(30)]
+        [TestCase(20)]
+        public void TestIfAttackMethodThrowsAnExceptionWhenOurHPIsLessThanOrEqualTo30(int hp)
+        {
+            Warrior warrior = new("Pesheto", 20, hp);
+            Warrior enemy = new("Gosheto", 20, 109);
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(enemy));
+        }
+
+        [Test]
+        [TestCase(30)]
+        [TestCase(20)]
+        public void TestIfAttackMethodThrowsAnExceptionWhenEnemyHPIsLessThanOrEqualTo30(int hp)
+        {
+            Warrior warrior = new("Pesho", 20, 100);
+            Warrior enemy = new("Gosho", 20, hp);
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(enemy));
+        }
+
+
+        [Test]
+        public void TestIfWarriorTriesToAttackAStrongerEnemy()
+        {
+            Warrior enemy = new("Gosho", 150, 100);
+            Assert.Throws<InvalidOperationException>(() => warrior.Attack(enemy));
+        }
     }
 }
